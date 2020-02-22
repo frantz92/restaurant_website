92596dc962232c18f24ba9b749e8f14ab361e3d6
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-02-22 20:12:00 +0100

    Add class CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -213,8 +213,8 @@
       thisWidget.value = settings.amountWidget.defaultValue;
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions();
-      ////console.log('AmountWidget', thisWidget);
-      ////console.log('constructor args:', element);
+      //console.log('AmountWidget', thisWidget);
+      //console.log('constructor args:', element);
     }
 
     getElements(element){
@@ -288,6 +288,33 @@
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       //console.log('DOM: ',generatedDOM);
       thisCart.dom.productList.appendChild(generatedDOM);
+      thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+      console.log('thisCart.products: ', thisCart.products);
+    }
+  }
+
+  class CartProduct{
+    constructor(menuProduct, element){
+      const thisCartProduct = this;
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+      thisCartProduct.getElements(element);
+      console.log('thisCartProduct: ', thisCartProduct);
+      console.log('productData: ', menuProduct);
+    }
+
+    getElements(element){
+      const thisCartProduct = this;
+      thisCartProduct.dom = {};
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
     }
   }
 
