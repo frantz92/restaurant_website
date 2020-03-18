4bcf48b48d84bb84d23a4b866319a03f4da81c5e
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-03-18 15:51:39 +0100

    Add submit function with address and phone

--- src/js/settings.js
+++ src/js/settings.js
@@ -63,6 +63,9 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    form: '.booking-form',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
   },
   nav: {
     links: '.main-nav a',
@@ -80,6 +83,7 @@ export const classNames = {
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
     active: 'active',
